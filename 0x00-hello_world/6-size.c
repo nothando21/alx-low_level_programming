8c33dd91e7b27ca6785cd505b9a0cc6e2a63ba0c
Nothando <nk.mashabane@gmail.com>
2022-09-08 13:46:03 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ long long int lli;
 char c;
 float f;
 
-printf("size of a char: %ld byte(s)\n", sizeof(c));
-printf("size of an int: %ld byte(s)\n", sizeof(i));
-printf("size of a long int: %ld byte(s)\n", sizeof(li);
-printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
-printf("size of a float: %ld byte(s)\n", sizeof(f));
+printf("size of a char: %d byte(s)\n", sizeof(c));
+printf("size of an int: %d byte(s)\n", sizeof(i));
+printf("size of a long int: %d byte(s)\n", sizeof(li);
+printf("size of a long long int: %d byte(s)\n", sizeof(lli));
+printf("size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
