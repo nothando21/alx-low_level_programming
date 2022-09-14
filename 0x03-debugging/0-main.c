945fb6887fa3214ecc1f99bb073907eee49f6d7f
Nothando <nk.mashabane@gmail.com>
2022-09-14 15:09:48 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,9 +2,7 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
- *
  */
 int main(void)
 {
@@ -12,5 +10,6 @@ int main(void)
 	
 	i = 98;
 	positive_or_negative(i);
+
 	return (0);
 }
