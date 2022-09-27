3449add2f813d2e0ed4d90b962ca3e7bc9c24ab8
Nothando <nk.mashabane@gmail.com>
2022-09-27 07:27:10 -0700

    task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0, n > 0, i++, n--)
-	{
+	for (i = 0, n > 0, i++)
 		dest[i] = src[i];
-	}
 
 	return (dest);
 }
