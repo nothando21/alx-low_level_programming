8118d3c4ca7ae726a269558109d53c9a165413f2
Nothando <nk.mashabane@gmail.com>
2022-09-28 07:29:46 -0700

    Task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
-#include "main.h”
+#include "main.h"
 
 /**
-* _puts_recursion - Prints a string, followed by a new line.
-* @s: The string to be printed
-*/
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: The string to be printed
+ */
 
 void _puts_recursion(char *s)
 {
