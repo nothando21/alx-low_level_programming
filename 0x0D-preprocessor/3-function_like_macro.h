1682eecd92dc9fa797b32d606eb2c45ffcf4e9ff
Nothando <nk.mashabane@gmail.com>
2022-10-10 12:16:39 -0700

    Task3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MARCO_H
 #define FUNCTION_LIKE_MARCO_H
 
-#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
+#define ABS(x) (x * (((x) < (0)) ? (-(x)) : (x))
 
 #endif /*FUNCTION_LIKE_MARCO_H*/
