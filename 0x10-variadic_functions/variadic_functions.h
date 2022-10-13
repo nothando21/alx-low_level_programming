c81712d2c4be36c0bc5b252bfe8becb312d1e2f2
Nothando <nk.mashabane@gmail.com>
2022-10-13 12:44:45 -0700

    Task0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,9 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+void _printchar(va_list list);
+void _printstr(va_list list);
+void _printfloat(va_list list);
+void _printint(va_list list);
 
 #endif /*VARIADIC_FUNCTION_H*/
