17c4dfe8a996135f75f0b7ed7502fcb19faf849d
Nothando <nk.mashabane@gmail.com>
2022-10-20 13:26:30 -0700

    Task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) print(void);
+/**
+ * print - This function prints a sentence before main is executed
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
