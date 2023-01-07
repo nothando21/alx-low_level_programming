7638f99b57e05413d191a977c2527cf6af55a5bd
Nothando <nk.mashabane@gmail.com>
2023-01-07 10:09:37 -0800

    fix

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -13,7 +13,7 @@ void shash_table_delete(shash_table_t *ht);
  * Return: If an error occurs - NULL.
  * Otherwise - a pointer to the new sorted hash table.
  */
-shash_table_t *shash_table_create(unsigned long int size)
+hash_table_t *shash_table_create(unsigned long int size)
 {
 	shash_table_t *ht;
 	unsigned long int i;
@@ -41,7 +41,7 @@ shash_table_t *shash_table_create(unsigned long int size)
  * @value: The value associated with key.
  *
  * Return: Upon failure - 0.
- * Otherwise - 1.
+ *         Otherwise - 1.
  */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
@@ -82,7 +82,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 		free(new);
 		return (0);
 	}
-	new->value = value_copy;
+	new->value = value_copy
 	new->next = ht->array[index];
 	ht->array[index] = new;
 
@@ -95,7 +95,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	}
 	else if (strcmp(ht->shead->key, key) > 0)
 	{
-		new->sprev = NULL
+		new->sprev = NULL;
 		new->snext = ht->shead;
 		ht->shead->sprev = new;
 		ht->shead = new;
@@ -119,12 +119,12 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 
 /**
  * shash_table_get - Retrieve the value associated with
- * a key in a sorted hash table.
+ *                   a key in a sorted hash table.
  * @ht: A pointer to the sorted hash table.
  * @key: The key to get the value of.
  *
  * Return: If the key cannot be matched - NULL.
- * Otherwise - the value associated with key in ht.
+ *         Otherwise - the value associated with key in ht.
  */
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
