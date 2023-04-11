8b1bc897662fb868539c46a8a0bf8ae6b7a839fd
Nothando <nk.mashabane@gmail.com>
2023-04-11 00:00:54 -0700

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,11 @@
 int binary_search(int *array, size_t size, int value);
 int _search(int *array, size_t beg, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
